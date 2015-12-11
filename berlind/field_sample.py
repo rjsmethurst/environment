bd4f1a3b72aca9cb1ebe060c34f29ea32459e441
Becky Smethurst <becky@MacBook-Air-2.local>
2015-12-11 12:02:13 +0000

    astropy 3d calc index of centrals that match closest in ra,dec,z to gz2-galex sample

--- berlind/field_sample.py
+++ berlind/field_sample.py
@@ -5,3 +5,15 @@ from astropy import units as u
 from astropy.cosmology import FlatLambdaCDM
 cosmo = FlatLambdaCDM(H0 = 71.0, Om0 = 0.26)
 
+groups = Table.read('mr181920_member_long_groupID_catalog_central_satellites_with_halo_mass_projected_clustercentric_radius_vel_disp_virial_radius_h_alpha_flux.fits', format='fits')
+cent = groups[N.where(groups['central or satellite']==1)]
+
+gz2 = Table.read('/Users/becky/Projects/Green-Valley-Project/data/GZ2_all_GALEX_match_GZ1_k_correct_green_valley.fits', format='fits')
+data = gz2[np.invert(np.in1d(gz2['ra_1'].data.data, groups['ra_1'].data.data))]
+zrange = 0.01 
+
+c = SkyCoord(ra=data['ra_1'], dec=data['dec_1'], distance=cosmo.luminosity_distance(data['z_1']), unit=(u.degree,u.degree,u.Mpc))
+catalog = SkyCoord(ra=cent['ra_1'], dec=cent['dec_1'],  distance=cosmo.luminosity_distance(cent['z_1']), unit=(u.degree,u.degree, u.Mpc))
+
+idx, d2d, d3d = c.match_to_catalog_3d(catalog)
+
